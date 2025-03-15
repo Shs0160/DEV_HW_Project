e4f82b1ff779e1a02e6b3ebdd924ff7d037bbe9e
Shs0160 <aladinsue@gmail.com>
2025-03-16 00:46:45 +0900

    프로젝트 생성 & create Post Class, Board Class

---
+++ src/main/java/board/post/Board.java
@@ -0,0 +1,30 @@
+package board.post;
+
+import java.util.ArrayList;
+
+public class Board {
+
+    //흐아 어렵다
+
+    //게시글을 한꺼번에 관리할 List 필요! -> 각각의 게시글은 번호를 가짐
+
+    //작성(Create), 조회(Reda), 수정(Update), 삭제(Delete)의 기능이 필요
+
+    private ArrayList<Post> post = new ArrayList<>();
+    private int postId = 1;
+
+    public void writePost(String title, String content) {
+        post.add(new Post(postId, title, content));
+        System.out.println("게시글이 작성이 완료되었습니다!");
+    }
+
+    public void readPost(int postId) {
+        for(Post post : post) {
+            if(post.id == postId){
+                System.out.println("제목: " + post.title);
+                System.out.println("내용: " + post.content);
+            }
+        }
+    }
+
+}
---
+++ src/main/java/board/post/Post.java
@@ -0,0 +1,14 @@
+package board.post;
+
+public class Post {
+
+    int id;
+    String title;
+    String content;
+
+    public Post(int id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+}
